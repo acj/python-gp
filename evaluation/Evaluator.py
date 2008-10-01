838772591eb7bc71fbf5ee67afbe24e12bc91217
Adam Jensen <ajensen@linuxguy.org>
2008-10-01 12:28:00 -0400

    - more skeleton files

---
+++ evaluation/Evaluator.py
@@ -0,0 +1,5 @@
+class Evaluator:
+	"""This is the base class for fitness evaluation mechanisms."""
+
+	def ComputeFitness(self, p):
+		pass
---
+++ program/TreeProgram.py
@@ -0,0 +1,13 @@
+class TreeProgram:
+	"""A representation of GP programs that uses a tree structure."""
+
+	def __init__(self, initial_tree=[]):
+		self.tree = initial_tree
+		pass
+
+	def ComputeResult(self):
+		pass
+
+	def PrintTree(self):
+		"""Traverses the tree and prints its structure to the screen"""
+		pass
