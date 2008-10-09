779b8e92a6e038675fb57bb149e72043ab0b4d22
Adam Jensen <ajensen@linuxguy.org>
2008-10-08 22:25:05 -0400

    - implementation of crossover and mutation

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ import program
 
 tally = 0
 count = 0
+max_fit = 0
+min_fit = 1
 for x in range(1,10):
 	prog = program.TreeProgram()
 	prog.RaiseTree()
 
 	# This is a known-working 3-mux tree:
-	#prog.tree = ['O','A','A','1','N','2','0','-1','-1','0']
+	#prog2 = program.TreeProgram()
+	#prog2.tree = ['O','A','A','1','N','2','0','-1','-1','0','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1','-1', '-1']
 
-	#print "Tree:"
+	#print "Trees:"
+	#print "\t" + prog.ToString()
+	#print "\t" + prog2.ToString()
+
+	#prog.Mutate(1, 0.25)
+	#print "Mutated tree:"
+	#print "\t" + prog.ToString()
+
+	#prog.CrossOver(prog2)
+	#print "CrossOver'd tree:"
 	#print "\t" + prog.ToString()
 
 	evaluator = evaluation.MultiplexerEvaluator()
@@ -19,5 +31,9 @@ for x in range(1,10):
 	print "[Run %i]: %s" % (x, str(program_fitness))
 	tally += program_fitness
 	count += 1
+	if program_fitness > max_fit:
+		max_fit = program_fitness
+	if program_fitness < min_fit:
+		min_fit = program_fitness
 
-print "[Avg] %f" % (float(tally)/count)
+print "[Avg] %f\t[Min] %f\t[Max] %f" % (float(tally)/count, min_fit, max_fit)
--- representation/BooleanFormulaTree.py
+++ representation/BooleanFormulaTree.py
@@ -10,6 +10,12 @@ class BooleanFormulaTree:
 		self.free_vars = free_vars
 		self.max_depth = max_depth
 		self.max_size = int(math.pow(2, max_depth))
+		# Initialize a vector of choices
+		self.choices = [ str(i) for i in range(0, self.free_vars) ]
+		# Include non-terminals
+		self.choices.append('A')
+		self.choices.append('O')
+		self.choices.append('N')
 
 	def GetMaxDepth(self):
 		return self.max_depth
@@ -22,45 +28,36 @@ class BooleanFormulaTree:
 		`AND', `OR', or `NOR' encoded as `A', `O', and `N' respectively, or 
 		terminals `0', `1', ..., `N-1', where N is the number of free variables 
 		in the Boolean equation."""
-
-		# Initialize a vector of choices
-		choices = [ str(i) for i in range(0, self.free_vars) ]
-
-		# Include non-terminals
-		choices.append('A')
-		choices.append('O')
-		choices.append('N')
 		index = 0
 		self.tree = [ '-1' for i in range(0, self.max_size + 1) ]
-
-		self.RaiseSubTree(index, choices)
+		self.RaiseSubTree(index)
 	
-	def RaiseSubTree(self, index, choices):
+	def RaiseSubTree(self, index):
 		if index <= self.max_size:
-			r = random.randint(0, len(choices)-1)
-			if choices[r] == 'A' and (2*index+2) <= self.max_size:
+			r = random.randint(0, len(self.choices)-1)
+			if self.choices[r] == 'A' and (2*index+2) <= self.max_size:
 				# Use AND
 				self.tree[index] = 'A'
-				self.RaiseSubTree(2*index + 1, choices)
-				self.RaiseSubTree(2*index + 2, choices)
-			elif choices[r] == 'O' and (2*index+2) <= self.max_size:
+				self.RaiseSubTree(2*index + 1)
+				self.RaiseSubTree(2*index + 2)
+			elif self.choices[r] == 'O' and (2*index+2) <= self.max_size:
 				# Use OR
 				self.tree[index] = 'O'
-				self.RaiseSubTree(2*index + 1, choices)
-				self.RaiseSubTree(2*index + 2, choices)
-			elif choices[r] == 'N' and (2*index+2) <= self.max_size:
+				self.RaiseSubTree(2*index + 1)
+				self.RaiseSubTree(2*index + 2)
+			elif self.choices[r] == 'N' and (2*index+2) <= self.max_size:
 				# Use NOT
 				self.tree[index] = 'N'
-				self.RaiseSubTree(2*index + 1, choices)
+				self.RaiseSubTree(2*index + 1)
 			else:
 				# Must be a terminal - remember to convert to string.  If we
 				# got here because our child indices would be out of bounds,
 				# then we need to pick a new random terminal.  Otherwise, use
 				# the terminal that is already selected.
 				if (2*index+2) > self.max_size:
-					self.tree[index] = choices[random.randint(0, self.free_vars-1)]
+					self.tree[index] = self.choices[random.randint(0, self.free_vars-1)]
 				else:
-					self.tree[index] = choices[r]
+					self.tree[index] = self.choices[r]
 		else:
 			print "Tried to raise a subtree past the array bounds"
 		
@@ -91,3 +88,38 @@ class BooleanFormulaTree:
 	def Pickle(self):
 		"""Return a string representation (pickled form) of ourselves"""
 		return self.tree
+	
+	def Mutate(self, num_mutations, mut_prob):
+		"""Perform mutation on the tree according to the given
+		parameters (number of mutations and the probability that mutation
+		should occur.  Currently, we allow mutations to unused parts of
+		the tree and do not prune unreachable branches that occur as a
+		result of mutation."""
+		for i in range(1, num_mutations+1):
+			print random.random()
+			if random.random() > mut_prob:
+				r = random.randint(0, len(self.tree)-1)
+				self.tree[r] = random.choice(self.choices)
+
+	def CrossOver(self, other_prog):
+		"""Performs a crossover of this program with the program taken
+		as a parameter.  Does a basic check to ensure that the max_depth
+		property of this program is not violated."""
+		xover_pt = 0
+		while True:
+			xover_pt = random.randint(1, len(self.tree)-1)
+			if xover_pt > len(other_prog.tree):
+				# Too high for the other tree's size; try again
+				continue
+			if self.tree[xover_pt] != -1 and other_prog.tree[xover_pt] != -1:
+				break
+		point_stack = [xover_pt]
+		while len(point_stack) > 0:
+			next_point = point_stack.pop()
+			if other_prog.tree[next_point] != '-1':
+				self.tree[next_point] = other_prog.tree[next_point]
+			if (2*next_point + 2) < len(other_prog.tree):
+				point_stack.append(2*next_point + 1)
+				point_stack.append(2*next_point + 2)
+		
+		# TODO: Check that we're not going to violate max_depth
