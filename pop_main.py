9360a062bad6ce2656b59b88b5feca1a94c000c9
Adam Jensen <ajensen@linuxguy.org>
2008-10-13 22:03:25 -0400

    - fixed crossover issue (needed to copy a list instead of assign it) - added pop_main driver

---
+++ pop_main.py
@@ -0,0 +1,21 @@
+import evaluation
+import representation
+import program
+import population
+
+tally = 0
+count = 0
+max_fit = 0
+min_fit = 1
+#for x in range(1,10):
+update_count = 0
+pop = population.Population(100, program.TreeProgram, evaluation.MultiplexerEvaluator)
+while True: #pop.best_fitness[1] < 1.0:
+	pop.DoStep()
+	update_count += 1
+	if (update_count % 1000) == 0:
+		print "== %i ==" % update_count
+#if pop.best_fitness[1] == 1.0:
+#	print "Resulting tree (index %i) after %i generations:" % (pop.best_fitness[0], update_count)
+#	print "\t%s" % pop.pop[pop.best_fitness[0]].ToString()
+#	print "\t%s" % pop.pop[pop.best_fitness[0]].Pickle()
