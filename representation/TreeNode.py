f459dd3fcd917b564feee680523d9444b72cdc25
Adam Jensen <ajensen@linuxguy.org>
2008-11-12 12:40:52 -0500

    - added missing TreeNode class

---
+++ representation/TreeNode.py
@@ -0,0 +1,12 @@
+class TreeNode:
+	def __init__(self):
+		pass
+
+	def GetArity(self):
+		return self.arity
+
+	def IsTerminal(self):
+		return self.terminal
+	
+	def GetValue(self):
+		return self.value
